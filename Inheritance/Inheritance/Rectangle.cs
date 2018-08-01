480e8bf617f6c735ef5c0b35049f42e5cb251baa
Vilde Vevatne <v-viveva@microsoft.com>
2018-08-01 14:12:50 -0700

    Add rectangle tests

--- Inheritance/Inheritance/Rectangle.cs
+++ Inheritance/Inheritance/Rectangle.cs
@@ -6,7 +6,25 @@ using System.Threading.Tasks;
 
 namespace Inheritance
 {
-    public class Rectangle
+    public class Rectangle : Shape
     {
+        public Point TopLeft { get; protected set; }
+        public double Width { get; protected set; }
+        public double Height { get; protected set; }
+        public double Area
+        {
+            get
+            {
+                return Width * Height;
+            }
+        }
+
+        public Rectangle(Point TopLeft, double Width, double Height)
+        {
+            this.TopLeft = TopLeft;
+            this.Width = Width;
+            this.Height = Height;
+            this.Name = "Rectangle";
+        }
     }
 }
--- Inheritance/InheritanceTests/RectangleTests.cs
+++ Inheritance/InheritanceTests/RectangleTests.cs
@@ -7,8 +7,30 @@ namespace InheritanceTests
     public class RectangleTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestNameSetProperly()
         {
+            // arrange
+            var topLeft = new Inheritance.Point(1, 2);
+            var height = 0.0;
+            var width = 0.0;
+            var expectedName = "Rectangle";
+            // act 
+            var rectangle = new Inheritance.Rectangle(topLeft, height, width);
+            // assert
+            Assert.AreEqual(rectangle.Name, expectedName);
+        }
+        [TestMethod]
+        public void TestAreaSetProperly()
+        {
+            // arrange
+            var topLeft = new Inheritance.Point(1, 2);
+            var height = 0.0;
+            var width = 0.0;
+            var expectedArea = 0.0;
+            // act 
+            var rectangle = new Inheritance.Rectangle(topLeft, height, width);
+            // assert
+            Assert.AreEqual(rectangle.Area, expectedArea);
         }
     }
 }
